08b15f7cae3a09f1def2a7f2749e6837309f27e1
Rodrigo Lessa <rodrigo@threeaccents.com>
2022-02-17 15:37:34 -0600

    dleta crdt

--- mix.exs
+++ mix.exs
@@ -23,7 +23,9 @@ defmodule Mahi.MixProject do
   defp deps do
     [
       {:horde, "~> 0.8.6"},
-      {:libcluster, "~> 3.3.1"}
+      {:libcluster, "~> 3.3.1"},
+      {:delta_crdt, "~> 0.6.4"}
+
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
