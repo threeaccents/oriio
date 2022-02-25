41400cd97a22b5046923d2678e610a704ed0a76f
Rodrigo Lessa <rodrigo@threeaccents.com>
2022-02-25 11:23:53 -0600

    use pid

--- apps/mahi/lib/mahi/uploads/chunk_upload_worker.ex
+++ apps/mahi/lib/mahi/uploads/chunk_upload_worker.ex
@@ -25,6 +25,7 @@ defmodule Mahi.Uploads.ChunkUploadWorker do
 
   @type chunk_number() :: non_neg_integer()
   @type document_path() :: binary()
+  @type io_device() :: File.io_device()
 
   @spec start_link(new_chunk_upload()) :: GenServer.on_start()
   def start_link(new_chunk_upload) do
@@ -48,7 +49,7 @@ defmodule Mahi.Uploads.ChunkUploadWorker do
     {:ok, state, {:continue, :load_state}}
   end
 
-  @spec append_chunk(pid(), {chunk_number(), document_path()}) :: :ok
+  @spec append_chunk(pid(), {chunk_number(), io_device()}) :: :ok
   def append_chunk(server, {chunk_number, chunk_document_path}) do
     GenServer.call(server, {:append_chunk, {chunk_number, chunk_document_path}})
   end
