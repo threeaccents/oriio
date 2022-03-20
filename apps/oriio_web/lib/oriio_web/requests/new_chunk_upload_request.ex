107d9cb098a436d09ef76f93a4586301ee198699
threeaccents <rodrigo@threeaccents.com>
2022-03-19 21:27:05 -0600

    add specs and docs

--- apps/oriio_web/lib/oriio_web/requests/new_chunk_upload_request.ex
+++ apps/oriio_web/lib/oriio_web/requests/new_chunk_upload_request.ex
@@ -1,13 +1,23 @@
 defmodule OriioWeb.NewChunkUploadRequest do
+  @moduledoc """
+  Validation parameters for creating a new chunk uplod.
+  """
+
   use Ecto.Schema
 
   import Ecto.Changeset
+
+  @type t() :: %__MODULE__{
+          file_name: String.t(),
+          total_chunks: integer()
+        }
 
   embedded_schema do
     field(:file_name, :string)
     field(:total_chunks, :integer)
   end
 
+  @spec from_params(map()) :: {:ok, t()} | {:error, Ecto.Changeset.t()}
   def from_params(params) do
     %__MODULE__{}
     |> cast(params, ~w/file_name total_chunks/a)
