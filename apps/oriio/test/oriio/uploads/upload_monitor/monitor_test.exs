fc9460756caf9fa9d9318f121c50c04f4f08d9b7
Van Damrongsri <van@threeaccents.com>
2022-03-30 18:15:54 -0400

    monitor test

--- apps/oriio/test/oriio/uploads/upload_monitor/monitor_test.exs
+++ apps/oriio/test/oriio/uploads/upload_monitor/monitor_test.exs
@@ -2,6 +2,9 @@ defmodule Oriio.Uploads.UploadMonitorTest do
   use Oriio.DataCase
 
   alias Oriio.Uploads.UploadMonitor
+  alias Oriio.Uploads.UploadMonitorSupervisor
+
+  alias Horde.DynamicSupervisor, as: DistributedSupervisor
 
   describe "start_link/1" do
     test "returns :ignore if process has already been started" do
@@ -25,34 +28,18 @@ defmodule Oriio.Uploads.UploadMonitorTest do
       # let the registry sync up
       :timer.sleep(1000)
 
-      # start_upload_monitor()
-
-      monitor_pid = Oriio.Debug.get_upload_monitor_pid()
+      monitor_pid = start_upload_monitor()
 
       original_node_with_monitor = node(monitor_pid)
 
-      IO.inspect("first")
-      IO.inspect(monitor_pid)
-      IO.inspect(original_node_with_monitor)
-
-      node_ref = Process.monitor(monitor_pid)
-
       :rpc.call(original_node_with_monitor, :init, :stop, [])
 
-      assert_receive {:DOWN, ^node_ref, :process, _, :normal}, 500
-
-      IO.inspect("killed node")
-
-      # # let the registry sync up
-      # :timer.sleep(1000)
+      # let the registry sync up
+      :timer.sleep(1000)
 
       monitor_pid = Oriio.Debug.get_upload_monitor_pid()
 
       new_node_with_monitor = node(monitor_pid)
-
-      IO.inspect("second")
-      IO.inspect(monitor_pid)
-      IO.inspect(new_node_with_monitor)
 
       assert new_node_with_monitor != original_node_with_monitor
 
@@ -61,16 +48,17 @@ defmodule Oriio.Uploads.UploadMonitorTest do
   end
 
   defp start_upload_monitor do
-    # let the registry sync up
-    :timer.sleep(500)
-
     monitor_pid = Oriio.Debug.get_upload_monitor_pid()
 
-    IO.inspect("start upload monitor")
-    IO.inspect(monitor_pid)
-
-    # make sure the monitor is not running on the current node as we will kill the node in the test.
+    # make sure the monitor is not running on the main node as we will kill the node in the test.
     if node(monitor_pid) == node() do
+      Process.exit(monitor_pid, :kill)
+
+      # let the registry sync up
+      :timer.sleep(1000)
+
+      DistributedSupervisor.start_child(UploadMonitorSupervisor, UploadMonitor)
+
       start_upload_monitor()
     else
       monitor_pid
