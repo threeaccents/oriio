ea6db46fe9cefffe61d3e7824b9d42299f6cafc2
threeaccents <rodrigo@threeaccents.com>
2022-07-25 19:20:34 -0600

    uploader app

---
+++ apps/uploader/lib/uploader.ex
@@ -0,0 +1,18 @@
+defmodule Uploader do
+  @moduledoc """
+  Documentation for `Uploader`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Uploader.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
