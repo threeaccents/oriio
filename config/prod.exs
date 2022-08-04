25349b2f74670b7785c0dfb5fa17b2e02f12c3fe
threeaccents <rodrigo@threeaccents.com>
2022-08-04 09:14:35 -0600

    clean up prod config

--- config/prod.exs
+++ config/prod.exs
@@ -10,32 +10,24 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 http_host = System.get_env("ORIIO_HOST") || "localhost"
-http_port = String.to_integer(System.get_env("ORIIO_PORT")) || 443
+http_port = String.to_integer(System.get_env("ORIIO_PORT")) || 80
 
 config :oriio_web, OriioWeb.Endpoint,
   url: [host: http_port, port: http_port],
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  https: [
-    port: http_port,
-    cipher_suite: :strong,
-    keyfile: System.get_env("ORIIO_SSL_KEY_PATH"),
-    certfile: System.get_env("ORIIO_SSL_CERT_PATH")
-  ],
-  force_ssl: System.get_env("ORIIO_FORCE_SSL") || true
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
-config :libcluster,
-  topologies: [
-    k8s_example: [
-      strategy: Elixir.Cluster.Strategy.Kubernetes,
-      config: [
-        mode: :dns,
-        kubernetes_node_basename: "oriio",
-        kubernetes_selector: "app=oriio,tier=backend",
-        kubernetes_namespace: "default",
-        polling_interval: 10_000
-      ]
-    ]
-  ]
+config :oriio_web,
+       :auth_secret_key,
+       System.get_env("ORIIO_AUTH_TOKEN") || raise("auth token is required")
+
+config :oriio,
+       :signed_upload_secret_key,
+       System.get_env("ORIIO_SIGNED_UPLOAD_SECRET_KEY") ||
+         raise("signed upload secret key is required")
+
+config :oriio, :file_storage, storage_engine: "local"
+
+config :oriio, :base_url, System.get_env("ORIIO_BASE_URL") || raise("oriio base url is required")
 
 # ## SSL Support
 # and clients may not be supported. You can set it to
