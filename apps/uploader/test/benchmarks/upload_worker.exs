b3eab468869797c7900ba35e0176a7aa9951da02
threeaccents <rodrigo@threeaccents.com>
2022-11-16 07:23:38 -0600

    test for multiple insets

--- apps/uploader/test/benchmarks/upload_worker.exs
+++ apps/uploader/test/benchmarks/upload_worker.exs
@@ -10,6 +10,10 @@ defmodule TO do
         pid
     end
   end
+
+  def rand_number() do
+    :rand.uniform(5_000)
+  end
 end
 
 {:ok, upload_id} = Uploader.new_upload("test.txt", 20_000)
@@ -20,8 +24,30 @@ test_chunk_file =
   "/home/threeaccents/work/threeaccents/code/threeaccents/oriio/data/fixtures/uploads/segmentaa"
 
 Benchee.run(%{
-  "order_map" => fn -> Uploader.UploadWorker.append_chunk_map(pid, 500, test_chunk_file) end,
-  "avl" => fn -> Uploader.UploadWorker.append_chunk_avl(pid, 500, test_chunk_file) end,
-  "bst" => fn -> Uploader.UploadWorker.append_chunk(pid, 500, test_chunk_file) end,
-  "list" => fn -> Uploader.UploadWorker.append_chunk_list(pid, 500, test_chunk_file) end
+  "single_order_map" => fn ->
+    Uploader.UploadWorker.append_chunk_map(pid, 500, test_chunk_file)
+  end,
+  "single_avl" => fn -> Uploader.UploadWorker.append_chunk_avl(pid, 500, test_chunk_file) end,
+  "single_bst" => fn -> Uploader.UploadWorker.append_chunk(pid, 500, test_chunk_file) end,
+  "single_list" => fn -> Uploader.UploadWorker.append_chunk_list(pid, 500, test_chunk_file) end,
+  "multiple_order_map" => fn ->
+    for iter <- 1..1000 do
+      Uploader.UploadWorker.append_chunk_map(pid, TO.rand_number(), test_chunk_file)
+    end
+  end,
+  "multiple_avl" => fn ->
+    for iter <- 1..1000 do
+      Uploader.UploadWorker.append_chunk_avl(pid, TO.rand_number(), test_chunk_file)
+    end
+  end,
+  "multiple_bst" => fn ->
+    for iter <- 1..1000 do
+      Uploader.UploadWorker.append_chunk(pid, TO.rand_number(), test_chunk_file)
+    end
+  end,
+  "multiple_list" => fn ->
+    for iter <- 1..1000 do
+      Uploader.UploadWorker.append_chunk_list(pid, TO.rand_number(), test_chunk_file)
+    end
+  end
 })
